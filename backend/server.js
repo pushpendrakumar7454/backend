398c99e3fbee9545908c8555576ae1df4fa60610
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-08-18 15:28:07 +0530

    basic

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("kya hua")
+let http = require("http")
+
+let server = http.createServer((req, res) => {
+    console.log("hey")
+    res.end("i get it")
+
+})
+
+server.listen(3000, () => {
+    console.log("port is running is 3000")
+})
