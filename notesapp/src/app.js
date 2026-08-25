1603d8419e9b38e741a03621644ed3044be8bf39
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-08-25 13:25:39 +0530

    router

--- notesapp/src/app.js
+++ notesapp/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const notesApp = require("./moduls/notes.moduls")
 const connectDb = require("./config/db")
+const createNotesControlles = require("./controlls/notes.controlles")
 const app = express()
 
 app.use(express.json())
@@ -9,7 +10,7 @@ app.get("/", (req, res) => {
     res.send("ok got it")
 })
 
-app.post("/create", )
+app.post("/create", createNotesControlles)
 
 
 module.exports = app;
--- notesapp/src/controlls/notes.controlles.js
+++ notesapp/src/controlls/notes.controlles.js
@@ -1,20 +1,20 @@
-const notesControlles = () => {
-    async(req, res) => {
-        try {
-            const { title, description } = req.body
-            const notes = await notesApp.create({
-                title,
-                description
-            })
+const notesApp = require("../moduls/notes.moduls")
 
-            return res.status(201).json({
-                message: "notes create succefully",
-                data: notes
-            })
-        } catch (error) {
-            console.log("error post", error)
-        }
+const createNotesControlles = async(req, res) => {
+    try {
+        const { title, description } = req.body
+        const notes = await notesApp.create({
+            title,
+            description
+        })
 
-
+        return res.status(201).json({
+            message: "notes create succefully",
+            data: notes
+        })
+    } catch (error) {
+        console.log("error post", error)
     }
 }
+
+module.exports = createNotesControlles
