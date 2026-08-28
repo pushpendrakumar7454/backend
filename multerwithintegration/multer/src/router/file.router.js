223bf6c302fb93e8558fb322ebfceee23cac9268
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-08-28 21:37:19 +0530

    memory

--- multerwithintegration/multer/src/router/file.router.js
+++ multerwithintegration/multer/src/router/file.router.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const upload = require("../config/multer")
 const createFileControllers = require("../controllers/file.controllers")
+const file = require("../config/multertwo")
 
 const router = express.Router()
 
-router.post("/", upload.single("image"), createFileControllers)
+router.post("/data", upload.single("image"), createFileControllers)
+router.post("/", file.single("image"), createFileControllers)
 
 module.exports = router
