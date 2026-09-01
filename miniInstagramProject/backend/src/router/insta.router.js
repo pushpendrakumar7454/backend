550fa498bde0def43b8a8dcd8e84e877ca8ab2d9
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-01 16:47:30 +0530

    Routers setup

--- miniInstagramProject/backend/src/router/insta.router.js
+++ miniInstagramProject/backend/src/router/insta.router.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const { createPostController } = require("../controllers/insta.controllers")
+const upload = require("../config/multer")
 
 const router = express.Router()
 
 
 
-router.post('/create', createPostController)
+router.post('/create', upload.single("image"), createPostController)
 module.exports = router
