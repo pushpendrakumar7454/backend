e1066fc8fc6e09f218a6a31f27e6d68e42587b4e
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-03 15:04:58 +0530

    middleware

---
+++ token/src/middleware/user.midleware.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken'
+import userModel from '../module/user.model'
+
+export const authHeader = async(req, res, next) => {
+
+    const token = req.headers.authorization
+
+    const data = jwt.decode(token)
+
+    const user = await userModel.findById(data.id)
+
+    req.user = user
+    next()
+}
