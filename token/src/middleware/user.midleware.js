d65db19e98beb9637bd59242b91d7ed7041cc03c
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-03 15:18:45 +0530

    middleware

--- token/src/middleware/user.midleware.js
+++ token/src/middleware/user.midleware.js
@@ -4,6 +4,11 @@ import userModel from '../module/user.model.js'
 export const authenticate = async(req, res, next) => {
 
     const token = req.headers.authorization
+    if (!token) {
+        return res.status(401).json({
+            message: "ttoken not found"
+        })
+    }
 
     const data = jwt.decode(token)
 
