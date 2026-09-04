2065fee6296e98e649f3e6e16cc1c08c38dfbe9a
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-04 14:54:38 +0530

    user Model

---
+++ accesTokenAndRefressToken/src/module/user.module.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    }
+})
+
+const userModel = mongoose.model("user1", userSchema)
+
+export default userModel
