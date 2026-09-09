82c52007eabdabfc8eeb546f611987049373b0f6
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-09 12:28:02 +0530

    database

---
+++ accessTokenAndRefreshToken/src/config/db.config.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+import { config } from './confg.js'
+const connectDb = async() => {
+    try {
+        await mongoose.connect(config.MONGO_URI)
+        console.log('mongo connected')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default connectDb
---
+++ accessTokenAndRefreshToken/src/server.js
@@ -0,0 +1,10 @@
+import app from "./app/app.js";
+import { config } from "./config/confg.js";
+import connectDb from "./config/db.config.js";
+const port = config.PORT;
+
+await connectDb()
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+});
