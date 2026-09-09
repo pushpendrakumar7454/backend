81933585fbf7e80121dbcf081274916ca8c4f85f
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-09 12:53:37 +0530

    config

--- accessTokenAndRefreshToken/src/config/confg.js
+++ accessTokenAndRefreshToken/src/config/confg.js
@@ -5,5 +5,7 @@ dotenv.config()
 
 export const config = {
     PORT: process.env.PORT,
-    MONGO_URI: process.env.MONGO_URI
+    MONGO_URI: process.env.MONGO_URI,
+    JWT_ACCESS_TOKEN: process.env.JWT_ACCESS_TOKEN,
+    JWT_REFRESS_TOKEN: process.env.JWT_REFRESS_TOKEN
 }
--- accessTokenAndRefreshToken/src/controllers/user.controllers.js
+++ accessTokenAndRefreshToken/src/controllers/user.controllers.js
@@ -1,5 +1,25 @@
+import userModel from "../module/user.module.js"
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import { config } from "../config/confg.js"
+
 export const registerUserController = async(req, res) => {
     try {
+        const { email, name, password } = req.body
+        const allredyExistUser = await userModel.findOne({ email })
+        if (allredyExistUser) {
+            return res.status(401).json({
+                message: "email allredy exist"
+            })
+        }
+
+        let token = jwt.sign({ id: user._id }, config.JWT_ACCESS_TOKEN)
+
+        const user = await userModel.create({
+            name,
+            email,
+            hashPassword: await bcrypt.hash(10, password)
+        })
 
     } catch (error) {
         res.status(500).json({
