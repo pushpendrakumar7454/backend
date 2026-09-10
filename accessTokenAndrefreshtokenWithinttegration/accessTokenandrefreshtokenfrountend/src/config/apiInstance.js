589d55ca681293e09bb042e68dbdf3fcbb5ea244
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-10 17:10:34 +0530

    apiInstance

--- accessTokenAndrefreshtokenWithinttegration/accessTokenandrefreshtokenfrountend/src/config/apiInstance.js
+++ accessTokenAndrefreshtokenWithinttegration/accessTokenandrefreshtokenfrountend/src/config/apiInstance.js
@@ -1,6 +1,28 @@
 import axios from "axios";
+import { useAuth } from "../context/authContext";
 
-export const apiInstance=axios.create({
-    baseURL:"http://localhost:3000/api",
-    withCredentials:true
-})
+export const apiInstance = axios.create({
+  baseURL: "http://localhost:3000/api",
+  withCredentials: true,
+});
+
+const useApi = () => {
+  const { accessToken } = useAuth();
+
+  apiInstance.interceptors.request.use(
+    (config) => {
+      if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+      }
+
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  return apiInstance;
+};
+
+export default useApi;
--- accessTokenAndrefreshtokenWithinttegration/accessTokenandrefreshtokenfrountend/src/context/authContext.jsx
+++ accessTokenAndrefreshtokenWithinttegration/accessTokenandrefreshtokenfrountend/src/context/authContext.jsx
@@ -1,14 +1,27 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 export const authContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
+const [user, setUser] = useState(null)
+const [accessToken, setAccessToken] = useState(null)
+const [loading, setLoading] = useState(true)
+
   return (
-    <authContext.Provider>
+    <authContext.Provider value={{user,setUser,accessToken,setAccessToken,loading,setLoading}}>
       {children}
     </authContext.Provider>
   );
 };
+
+
+export const useAuth=()=>{
+  const context=useContext(authContext)
+  if(!context){
+    throw new Error ("useAuth must be used within an authProvider")
+  }
+  return context
+}
 
 export default AuthContextProvider;
 
