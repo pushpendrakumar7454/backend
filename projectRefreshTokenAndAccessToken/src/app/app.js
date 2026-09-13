3aa281f1da77e93f681c0dc3357b3d36f3223f50
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-13 13:45:55 +0530

    deleted

--- projectRefreshTokenAndAccessToken/src/app/app.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import authRouter from '../router/auth.router.js'
-
-const app=express()
-app.use(express.json())
-
-
-app.use("/api/auth",authRouter)
-export default app
