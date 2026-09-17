898b779379482f4a65368d5fbd7296bfd3a503aa
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-17 21:13:36 +0530

    validation setup

---
+++ express_validation/src/modules/user.module.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+
+const userSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    number:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const userModel=mongoose.model("validationExpress",userSchema)
+
+export default userModel
