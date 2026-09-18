44be430d8d3a9e5f9567218a5efe97b194bae0a2
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-18 16:37:47 +0530

    controllers

--- practiceExpressValidation/src/controllers/user.controllers.js
+++ practiceExpressValidation/src/controllers/user.controllers.js
@@ -1,5 +1,6 @@
 import userModel from "../modules/user.module.js"
 import bcypt from 'bcrypt'
+import { generateUserToken } from "../utils/auth.js"
 export const userRegisterController=async(req,res)=>{
     try {
          const {email,number,password}=req.body
@@ -18,6 +19,22 @@ export const userRegisterController=async(req,res)=>{
             number,
             password:await bcypt.hash(password,8)
          })
+
+       const{accessToken,refreshToken}= generateUserToken({userId:user._id})
+
+       user.refreshToken=refreshToken
+       await user.save()
+
+       res.cookie("refreshToken",refreshToken,{
+        httpOnly:true
+       })
+
+       return res.status(201).json({
+        message:"user reggister succefully",
+        data:user,
+        accessToken
+       })
+
     } catch (error) {
         return res.status(500).json({
             message:"internal server error"
