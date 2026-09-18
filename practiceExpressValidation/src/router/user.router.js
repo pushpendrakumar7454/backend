80b81d5860471f71c86d73b31ac45e20bbc0b48b
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-18 16:39:24 +0530

    router

--- practiceExpressValidation/src/router/user.router.js
+++ practiceExpressValidation/src/router/user.router.js
@@ -1,6 +1,10 @@
 import  {Router} from 'express'
+import { registerUserValidator } from '../validator/user.validate.js'
+import { userRegisterController } from '../controllers/user.controllers.js'
 
 const router=Router()
+
+router.post("/register",registerUserValidator,userRegisterController)
 
 export default router
 
