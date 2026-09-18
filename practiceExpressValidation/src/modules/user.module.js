c15fda5ccb151b9c478c35995e17a9ff3d2a083b
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-18 16:23:22 +0530

    user Model

---
+++ practiceExpressValidation/src/modules/user.module.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+
+const userSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },number:{
+      type:String,
+      required:true,
+      unique:true
+    },password:{
+        type:String,
+        required:true
+    },refreshToken:{
+        type:String
+    }
+})
+
+const userModel=mongoose.model("paracticeUserExpressVAlidation",userSchema)
+
+export default userModel;
