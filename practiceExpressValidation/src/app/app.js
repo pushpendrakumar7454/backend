5be1ce3eedc44f4f73a173b545052d52874de868
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-18 16:34:52 +0530

    app

--- practiceExpressValidation/src/app/app.js
+++ practiceExpressValidation/src/app/app.js
@@ -1,5 +1,10 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+import userRouter from '../router/user.router.js'
 
 const app=express()
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth",userRouter)
 
 export default app;
