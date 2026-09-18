149ec9820484997f7c6a7ac6bde0ed2e8c31c287
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-18 21:20:14 +0530

    userModel

---
+++ e-coamrseProject/backend/src/modules/user.modulee.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+const userSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    hashPassword:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        default:'user',
+        enum:['seller','user']
+    }
+})
+
+const userModel=mongoose.model("E-comarseFisrtProject",userSchema)
+
+export default userModel
