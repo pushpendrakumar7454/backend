65933c75a60392dcbb1955418a750dd6b3fda1c6
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-19 17:39:12 +0530

    user model

---
+++ practiceProject3E-comarse/backend/src/modules/auth.module.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+
+const authSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    name:{
+        type:String,
+        required:true,
+
+    },
+    hashPassword:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        default:"user",
+        enum:['user','seller']
+    }
+})
+
+const userModel=mongoose.model("project3PracticeEcomarseProject",authSchema)
+
+export default userModel
