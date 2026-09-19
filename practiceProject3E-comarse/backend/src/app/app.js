e36abf4e794c70dd4f06d92a21a4d3c8c6e08f1f
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-19 18:13:57 +0530

    app

--- practiceProject3E-comarse/backend/src/app/app.js
+++ practiceProject3E-comarse/backend/src/app/app.js
@@ -1,8 +1,13 @@
-import express from 'express'
-import cookieParser from 'cookie-parser'
-const app=express()
-app.use(express.json())
-app.use(cookieParser())
+import express from "express";
+import cookieParser from "cookie-parser";
+import authRouter from "../router/auth.router.js";
 
-app.use("/api/auth",)
-export default app
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRouter);
+
+export default app;
