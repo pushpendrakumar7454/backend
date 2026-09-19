602235361f2d2cd36b42a6689e1d85a52f4db3bf
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-19 18:09:48 +0530

    controler

--- practiceProject3E-comarse/backend/src/app/app.js
+++ practiceProject3E-comarse/backend/src/app/app.js
@@ -1,5 +1,8 @@
 import express from 'express'
-
+import cookieParser from 'cookie-parser'
 const app=express()
+app.use(express.json())
+app.use(cookieParser())
 
+app.use("/api/auth",)
 export default app
--- practiceProject3E-comarse/backend/src/controllers/auth.controllers.js
+++ practiceProject3E-comarse/backend/src/controllers/auth.controllers.js
@@ -1,3 +1,54 @@
+import userModel from "../modules/auth.module.js"
+import bycpt from 'bcryptjs'
+import { generateAccesToken, generateRefreshToken } from "../utils/auth"
+
 const authRegisterController=async()=>{
-    
+    try {
+        const {name,email,password,role}=req.body
+
+        const allreadyExistEmail=await userModel.findOne("email")
+
+        if(allreadyExistEmail){
+            return res.status(400).json({
+                message:"email allready exiits"
+            })
+        }
+
+        const user=await userModel({
+            name,
+            email,
+            hashPassword:await bycpt.hash(password,6),
+            role
+        })
+
+        const {accessToken}=generateAccesToken({userId:user._id,role})
+        const {refreshToken}=generateRefreshToken({userId:user._id,role})
+          
+        user.refreshToken=refreshToken
+        await user.save()
+
+        res.cookie("refreshToken",refreshToken,({
+            httpOnly:true
+        }))
+
+        return res.status(201).json({
+            message:"user register succefully",
+            data:{
+                user:{
+                    name:user.name,
+                    email:user.email,
+                    id:user._id,
+                    role:user.role
+                }
+            },
+            accessToken
+        })
+
+
+    } catch (error) {
+        return res.status(500).json({
+            meessagee:"internal server error"
+        })
+    }
+
 }
