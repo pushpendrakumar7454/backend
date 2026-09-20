03e75e0f10004f13924db4ea42cdeb7c2e5ac166
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-20 16:22:26 +0530

    middleware

--- e-coamrseProject/backend/src/middleware/auth.middleware.js
+++ e-coamrseProject/backend/src/middleware/auth.middleware.js
@@ -1,50 +1,26 @@
-import userModel from "../modules/user.modulee.js";
-import { readaccessToken } from "../utils/auth.js";
+import { readAccessToken } from "../../../../practiceProject3E-comarse/backend/src/utils/auth.js";
 
-export const authenticate = async (req, res, next) => {
+const authenticate=async(req,res,next)=>{
   try {
-    const authHeader = req.headers.authorization
+      
+     const accessToken = req.headers.authorization?.split(" ")[1];
 
-    if (!authHeader) {
-      return res.status(401).json({
-        message: "access token is required",
-      });
-    }
+     if(!accessToken){
+      return res.status(400).json({
+        message:"accessToken not found"
+      })
+     }
 
-    const accessToken = authHeader.split(" ")[1];
+     const decoded=readAccessToken(accessToken)
+     req.user=decoded
+     nex()
 
-    if (!accessToken) {
-      return res.status(401).json({
-        message: "access token is wrong",
-      });
-    }
 
-    const decoded = readaccessToken(accessToken);
-
-    const user = await userModel.findById(decoded.userId);
-
-    if (!user) {
-      return res.status(401).json({
-        message: "user not found",
-      });
-    }
-
-    if (!user) {
-      return res.status(401).json({
-        message: "user not found",
-      });
-    }
-
-    req.user = user;
-
-    next();
   } catch (error) {
-    console.log(error);
-
-    return res.status(401).json({
-      message: "token expired or invalid",
-    });
+    return res.status(500).json({
+      message:"token expire"
+    })
   }
-};
+}
 
 export default authenticate;
