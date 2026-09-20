3c85f76b85cd60b2f3a525f5c104a35849e76754
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-20 15:30:29 +0530

    controller

--- e-coamrseProject/backend/src/controllers/auth.controller.js
+++ e-coamrseProject/backend/src/controllers/auth.controller.js
@@ -98,7 +98,7 @@ export const authLoginControllers = async (req, res) => {
     });
 
     // Save refresh token
-    await userModel.findByIdAndUpdate(user._id, { refreshToken });
+    await userModel.findOneAndUpdate({email}, { refreshToken });
 
     // Set refresh token cookie
     res.cookie("refreshToken", refreshToken, {
--- practiceProject3E-comarse/backend/src/validator/auth.validator.js
+++ practiceProject3E-comarse/backend/src/validator/auth.validator.js
@@ -3,27 +3,27 @@ import { body, validationResult } from "express-validator";
 export const registerValidator = [
   body("email")
     .exists()
-    .withMessage("email is required")
+    .withMessage("email is required").bail()
     .trim()
     .isEmail()
     .withMessage("please enter valid formate of email"),
 
   body("name")
     .exists()
-    .withMessage("name is required")
+    .withMessage("name is required").bail()
     .isString()
-    .withMessage("please enter the password in string")
+    .withMessage("please enter the password in string").bail()
     .isLength({ min: 3, max: 50 })
     .withMessage(
       "password at least minimum 3 character and 50 maximum character",
-    )
+    ).bail()
     .trim(),
 
   body("password")
     .exists()
-    .withMessage("password is required")
+    .withMessage("password is required").bail()
     .isString()
-    .withMessage("please enter password in string")
+    .withMessage("please enter password in string").bail()
     .trim()
     .isLength({ min: 6 })
     .withMessage("password is required at least 6 character"),
@@ -40,5 +40,31 @@ export const registerValidator = [
     next();
   },
 ];
+
+
+export const loginControllers=[
+  body("email")
+  .exists().withMessage("email is required").bail()
+  .isEmail().withMessage("please enter email valid formate")
+  .isString().withMessage("email must be string")
+  .trim(),
+
+  body("password")
+  .exists().withMessage("email is required")
+  .isString().withMessage("password must be string")
+  .isLength({min:6}).withMessage("password must be 6 character required")
+  .trim(),
+
+  (req,res,next)=>{
+    const errors=validationResult(req)
+    if(!errors.isEmpty()){
+      return res.status(400).json({
+        message:"invalid request",
+        errors:errors.array()
+      })
+    }
+    next()
+  }
+]
 
 
