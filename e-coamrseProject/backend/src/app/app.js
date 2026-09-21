23f39df793e5f9afdb5a8734d5211459428f810c
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-21 21:45:01 +0530

    user model

--- e-coamrseProject/backend/src/app/app.js
+++ e-coamrseProject/backend/src/app/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import authRouter from '../router/auth.router.js'
 import cookeParser from 'cookie-parser'
+import productRouter from '../router/product.route.js'
 
 
 const app=express()
@@ -8,5 +9,6 @@ app.use(express.json())
 app.use(cookeParser())
 
 app.use("/api/auth",authRouter)
+app.use("/api/products",productRouter)
 
 export default app
---
+++ e-coamrseProject/backend/src/modules/product.model.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+
+    title: {
+        type: String,
+        required: true,
+        minLength: 2,
+        maxLength: 100
+    },
+
+    description: {
+        type: String,
+        required: true,
+        minLength: 10,
+        maxLength: 500
+    },
+
+    images: {
+        type: [String],
+        validate: {
+            validator: images => images.length <= 5,
+            message: "A product can have at most 5 images"
+        }
+    },
+
+    price: {
+        amount: {
+            type: Number,
+            required: true
+        },
+        currency: {
+            type: String,
+            enum: ["INR", "USD"],
+            default: "INR"
+        }
+    },
+
+    sizes: [
+        {
+       size:{
+            type: String,
+            enum: ["XS", "S", "M", "L", "XL", "XXL"],
+            required: true,
+        },
+        stock: {
+                type: Number,
+                min: 0,
+                default: 0
+            }
+        } 
+    ],
+
+    seller: {
+        type: mongoose.Types.ObjectId,
+        ref: "users",
+        required: true
+    }
+
+});
+
+const productModel = mongoose.model(
+    "productsCollection",
+    productSchema
+);
+
+export default productModel;
