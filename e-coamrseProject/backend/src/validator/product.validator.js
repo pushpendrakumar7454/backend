1d887aab5fa3d06431ecd567b4f9cfc0ce9b5ead
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-22 21:19:41 +0530

    product validation

--- e-coamrseProject/backend/src/validator/product.validator.js
+++ e-coamrseProject/backend/src/validator/product.validator.js
@@ -7,7 +7,7 @@ export const createProductValidtar=[
     .isString().withMessage("title must be string").bail()
     .trim()
     .isLength({min:2,max:100}).withMessage("title length must be between 2 to 100 character").bail()
-    .isAlpha("en-Us",{ignore:" "}).withMessage("title can only have englsh small case and capital case character"),
+    .isAlpha("en-Us",{ignore:" -"}).withMessage("title can only have englsh small case and capital case character"),
 
     body("description")
     .exists().withMessage("des is required").bail()
@@ -30,6 +30,7 @@ export const createProductValidtar=[
 
    body("sizes.*.size")
    .exists().withMessage("size is requird").bail()
+   .trim()
    .isString().withMessage("size must be string").bail()
    .isIn(["XS", "S", "L", "XL", "XXL"]).withMessage("sizes must s,xs,l,xl,xxl"),
 
