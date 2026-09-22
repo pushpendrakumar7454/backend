a5be7dc972eca8087f9182f08b8a12a15021982b
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-22 15:48:07 +0530

    product validator

--- practiceProject3E-comarse/backend/src/validator/product.validator.js
+++ practiceProject3E-comarse/backend/src/validator/product.validator.js
@@ -1,13 +1,28 @@
-import {body} from 'express-validator'
-
+import {body,validationResult} from 'express-validator'
 
 export const createProductValidator=[
     body("title")
-    .exists().withMessage("title must be required").bail()
+    .exists().withMessage("title is required").bail()
+    .isString().withMessage("title must be Strring").bail()
     .trim()
-    .isString().withMessage("title must be String").bail()
-    .isLength({min:2,max:100}).withMessage("title must be eat least minimum 2 character and maximum 100 character")
-    .isAlpha("en-US",{ignore:" "}).withMessage("title is must me small letters and capital letters"),
+    .isLength({min:2,max:100}).withMessage("title must be required minimum 2 character and maximum 100 character").bail()
+    .isAlpha("en-US",{ignore:" "}).withMessage("title can only have small letters and capital letters"),
 
     body("desctiption")
+    .exists().withMessage("desctiption is required").bail()
+    .isLength({min:10,max:500}).withMessage("desctiption must be minimum 10 character and maximum 500 character").bail()
+    .trim()
+    .isString().withMessage("desctiption must be String"),
+
+    body("price.amount")
+    .exists().withMessage("price is required").bail()
+    .isFloat({min:0}).withMessage("amount must be float"),
+
+    body("price.currency")
+    .exists().withMessage("current is required").bail()
+    .isString().withMessage("currency must be string").bail()
+    .isIn(["INR","USD"]).withMessage("currency either be uSD AND INR")
+
+
+
 ]
