29a70382fafe878ab7db5313475902af4f62b406
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-23 10:20:13 +0530

    app

---
+++ BasicEcomraseProjectWithFullIntegration/backend/src/controllers/auth.controllers.js
@@ -0,0 +1,9 @@
+export const authRegisterController=async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        return res.status(500).json({
+            message:"internal server error"
+        })
+    }
+}
