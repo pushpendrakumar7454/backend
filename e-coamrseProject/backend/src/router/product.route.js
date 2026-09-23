ca6cefecbcff0db2db4f7ba3d843dbb4b18e60e4
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-23 20:59:15 +0530

    router

--- e-coamrseProject/backend/src/router/product.route.js
+++ e-coamrseProject/backend/src/router/product.route.js
@@ -1,5 +1,15 @@
 import {Router} from 'express'
-
+import authenticate from '../middleware/auth.middleware.js'
 const router=Router()
+
+
+router.post("/",authenticate,(req,res,next)=>{
+    if(req.user.role=="seller"){
+       return res.status(403).json({
+        message:"user not authorized create product"
+       })
+    }
+    next()
+},)
 
 export default router
