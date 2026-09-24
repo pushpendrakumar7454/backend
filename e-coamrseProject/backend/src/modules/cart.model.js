94d585134a2ff9d2006b2c027064d97c0649e6d1
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-24 22:06:20 +0530

    controller

--- e-coamrseProject/backend/src/modules/cart.model.js
+++ e-coamrseProject/backend/src/modules/cart.model.js
@@ -1,1 +1,27 @@
 import mongoose from "mongoose";
+
+const cartSchema = new mongoose.Schema({
+  products: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+        min: 1,
+      },
+      size: {
+        type: String,
+        enum: ["XS", "S", "M", "L", "XL", "XXL"],
+      },
+    },
+  ],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+});
+
+const cartModel=mongoose.model("cartModelOfEmarse",cartSchema)
