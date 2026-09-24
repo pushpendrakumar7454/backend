1024e55e032b2b03c692bad44955e19459c3d36c
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-24 21:48:42 +0530

    controller

--- e-coamrseProject/backend/src/controllers/product.controllers.js
+++ e-coamrseProject/backend/src/controllers/product.controllers.js
@@ -1,48 +1,45 @@
 import productModel from "../modules/product.model.js";
 import { uploadFiles } from "../services/storage.service.js";
 
-
 export const createProductCoontroller = async (req, res) => {
-    try {
-        console.log("BODY:", req.body);
-        console.log("FILES:", req.files);
+  try {
+    console.log("BODY:", req.body);
+    console.log("FILES:", req.files);
 
-        const fileUrl = [];
+    const fileUrl = [];
 
-        for (let i = 0; i < req.files.length; i++) {
-            const response = await uploadFiles({
-                buffer: req.files[i].buffer,
-                fileName: req.files[i].originalname
-            });
+    for (let i = 0; i < req.files.length; i++) {
+      const response = await uploadFiles({
+        buffer: req.files[i].buffer,
+        fileName: req.files[i].originalname,
+      });
 
-            console.log("IMAGEKIT RESPONSE:", response);
+      console.log("IMAGEKIT RESPONSE:", response);
 
-            fileUrl.push(response.url);
-        }
-
-        const product = await productModel.create({
-    title: req.body.title,
-    description: req.body.description,
-    price: {
-        amount: req.body.price.amount,
-        currency: req.body.price.currency
-    },
-    sizes: req.body.sizes,
-    images: fileUrl,
-    seller: req.user.userId
-});
-
-        return res.status(201).json({
-            message: "Product created successfully",
-            data: product
-        });
-
-    } catch (error) {
-
-        console.log("CREATE PRODUCT ERROR:", error);
-
-        return res.status(500).json({
-            message: "internal server error"
-        });
+      fileUrl.push(response.url);
     }
+
+    const product = await productModel.create({
+      title: req.body.title,
+      description: req.body.description,
+      price: {
+        amount: req.body.price.amount,
+        currency: req.body.price.currency,
+      },
+      sizes: req.body.sizes,
+      images: fileUrl,
+      seller: req.user.userId,
+    });
+
+    return res.status(201).json({
+      message: "Product created successfully",
+      data: product,
+    });
+  } catch (error) {
+    console.log("CREATE PRODUCT ERROR:", error);
+
+    return res.status(500).json({
+      message: "internal server error",
+    });
+  }
 };
