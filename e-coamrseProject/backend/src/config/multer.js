dc0dc85cbee75279a8df417136d555aea66e42a2
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-24 15:21:49 +0530

    multer

---
+++ e-coamrseProject/backend/src/config/multer.js
@@ -0,0 +1,13 @@
+import multer from 'multer'
+
+const storage=multer.memoryStorage()
+
+const upload=multer({
+    storage,
+    limits:{
+        files:5,
+        fileSize:1*2024*2024
+    }
+})
+
+export default upload
