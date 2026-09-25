00c950f176a9cc80cdbb9a54c293cbf8dafda8ff
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-25 21:07:29 +0530

    app

--- e-coamrseProject/backend/src/app/app.js
+++ e-coamrseProject/backend/src/app/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import authRouter from '../router/auth.router.js'
 import cookeParser from 'cookie-parser'
 import productRouter from '../router/product.route.js'
+import cartRouter from '../router/cart.router.js'
 
 
 const app=express()
@@ -9,6 +10,12 @@ app.use(express.json())
 app.use(cookeParser())
 
 app.use("/api/auth",authRouter)
+
+
 app.use("/api/products",productRouter)
+
+
+app.use("/api/cart",cartRouter)
+
 
 export default app
