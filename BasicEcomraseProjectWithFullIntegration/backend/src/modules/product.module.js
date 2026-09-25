f984528dabbd62662db0c1c6fc6829b47e73d6d3
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-25 07:23:43 +0530

    product model

---
+++ BasicEcomraseProjectWithFullIntegration/backend/src/modules/product.module.js
@@ -0,0 +1,57 @@
+import mongoose from 'mongoose'
+
+const productSchema=new mongoose.Schema({
+    title:{
+      type:String,
+      required:true,
+      minLength:2,
+      maxLength:50
+    },description:{
+        type:String,
+        required:true,
+        minLength:10,
+        maxLength:500
+    },
+    images:{
+        type:[
+            {
+                type:String
+            }
+        ],
+        validate:{
+            validator:images=>images.length<=5,
+            message:"images must be at least miximum 5"
+        }
+    },
+    price:{
+        amount:{
+        type:Number,
+        required:true
+        },
+        currency:{
+         type:String,
+         required:true,
+         enum:["USD","INR"],
+         default:"INR"
+        }
+    },
+    sizes:[
+        {
+            size:{
+              type:String,
+              required:true
+            },
+            stock:{
+              type:Number,
+              min:0,
+              default:0
+
+            }
+        }
+    ]
+
+}) 
+
+const productModel=mongoose.model("basicEcomarseProductModel",productSchema)
+
+export default productModel;
