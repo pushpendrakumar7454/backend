2baaf331ef6b499af1b7f33a35c1ad8709e932f3
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-25 21:03:22 +0530

    cart validator

---
+++ e-coamrseProject/backend/src/validator/cart.validator.js
@@ -0,0 +1,31 @@
+import { body, validationResult } from "express-validator";
+
+
+export const createCartProduct=[
+    body("productId")
+    .exists().withMessage("product id is required").bail()
+    .isString().withMessage("product is must be string").bail()
+    .trim()
+    .isMongoId().withMessage("product must be a valid mongo Id"),
+
+    body("quantuty")
+    .exists().withMessage("quantuty is required").bail()
+    .isInt({min:1}).withMessage("quantuty must be a integer greter than 0"),
+
+    body("size")
+    .exists().withMessage("size is required").bail()
+    .isString().withMessage("size must be string").bail()
+    .isIn(["XS", "S", "M", "L", "XL", "XXL"]).withMessage("size must be XS, S, M, L, XL or XXL"),
+
+    (req,res,next)=>{
+        const errors=validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                message:"invalid request",
+                errors:errors.array()
+            })
+        }
+        next()
+    }
+
+]
