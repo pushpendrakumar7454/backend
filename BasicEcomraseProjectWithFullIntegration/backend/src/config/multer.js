ea3d1d6c8d9e2d4edd16855fb23b769d983a3c69
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-25 07:46:03 +0530

    multer

---
+++ BasicEcomraseProjectWithFullIntegration/backend/src/config/multer.js
@@ -0,0 +1,13 @@
+import multer from 'multer'
+
+const storage=multer.memoryStorage()
+
+const upload=multer({
+    storage,
+    limits:{
+        files:5,
+        fileSize:1*1024*1024
+    }
+})
+
+export default upload;
