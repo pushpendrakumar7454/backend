61917e02623a949d7d68bf861da3953857773e08
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-25 22:01:57 +0530

    controllers

--- e-coamrseProject/backend/src/controllers/cart.controller.js
+++ e-coamrseProject/backend/src/controllers/cart.controller.js
@@ -1,4 +1,5 @@
 import productModel from "../modules/product.model.js";
+import cartModel from "../modules/cart.model.js";
 
 export const createCartController = async (req, res) => {
   try {
@@ -24,6 +25,28 @@ export const createCartController = async (req, res) => {
         message: "insufficient stock",
       });
     }
+
+   const cart=(await cartModel.findOne({user:req.user.userId}))
+    ?? await cartModel.create({user:req.user.userId})
+
+
+    const isProductInCart=cart.products.find(p=>(p.product.toString()===productId) && (p.size===size))
+
+    if(isProductInCart){
+        if((isProductInCart.quantity+quantity)>selectedsizes.stock){
+            return res.status(400).json({
+                message:"insufficient stock"
+            })
+        }
+
+
+        await cartModel.updateOne({
+            user:req.user.userId,
+            "products.product":productId,
+            "products.size":size
+        })
+    }
+
   } catch (error) {
     return res.status(500).json({
       message: "internal servver error",
