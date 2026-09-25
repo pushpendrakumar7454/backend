014e62c01eb7ff5c5d0c35c5382eb60a8917cb83
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-25 15:45:52 +0530

    services

--- practiceProject3E-comarse/backend/src/services/service.storage.js
+++ practiceProject3E-comarse/backend/src/services/service.storage.js
@@ -1,6 +1,17 @@
-import imageKit from '@imagekit/nodejs'
+import ImageKit, { toFile } from '@imagekit/nodejs'
+import {config} from '../config/config.js'
 
 
-const client = new imageKit({
-    
+const client=new ImageKit({
+    privateKey:config.IMAGEKIT_PRIVATE_KEY
 })
+
+
+export const uploadFiles=async({buffer,fileName})=>{
+    const responce=await client.files.upload({
+        file:await toFile(buffer),
+        fileName:fileName,
+        folder:"e-comrase"
+    })
+    return responce
+}
